35ca13b740fb6331cbb83182a6ae38d6e2edb09c
Simon Asika <asika32764@gmail.com>
2013-11-12 14:17:51 +0000

    Add console setting

--- cli/console.php
+++ cli/console.php
@@ -8,4 +8,21 @@ use Joomla\Console\Output\Stdout;
 
 $console = new Console(null, null, new Stdout);
 
+$console
+	// ->setName('Example Console')
+	// ->setVersion('1.0')
+	// ->setUsage('console <command> [option]')
+	// ->setDescription('Hello World')
+	/* ->setHelp(
+		<<<HELP
+Hello, this is an example console, if you want to do something, see above:
+
+$ foo bar -h => foo bar --help
+
+---------
+
+$ foo bar yoo -q => foo bar yoo --quiet
+HELP
+	)*/;
+
 $console->execute();
